70452fb410122889b2065d9691c17f19517a9d01
FarhajQureshi <farhazqureshi934016@gmail.com>
2025-05-31 23:47:07 +0530

    tird commit

--- src/components/forms/QuizCreation.tsx
+++ src/components/forms/QuizCreation.tsx
@@ -12,7 +12,7 @@ import {
 import { quizCreationSchema } from "@/schemas/quiz";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Form,FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 
@@ -37,10 +37,24 @@ const QuizCreation = (props: Props) => {
         </CardHeader>
         <CardContent>
           <Form {...form}>
-            <form className="space-y-8">
-                  <FormField control={form.control} name="username" render={({})}>
-
-                  </FormField>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
             </form>
           </Form>
         </CardContent>

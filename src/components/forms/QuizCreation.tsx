857c37a72456d4e4ab67fd8d20a3db24022ae827
FarhajQureshi <farhazqureshi934016@gmail.com>
2025-05-31 23:57:19 +0530

    fouth commit

--- src/components/forms/QuizCreation.tsx
+++ src/components/forms/QuizCreation.tsx
@@ -15,6 +15,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { BookOpen, CopyCheck } from "lucide-react";
+import { Separator } from "@radix-ui/react-dropdown-menu";
 
 type Props = {};
 type Input = z.infer<typeof quizCreationSchema>;
@@ -28,6 +30,11 @@ const QuizCreation = (props: Props) => {
       amount: 3,
     },
   });
+
+  const onSubmit = async (input: Input) => {
+    alert(JSON.stringify(input, null, 2));
+
+  }
   return (
     <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[500px]">
       <Card>
@@ -40,20 +47,76 @@ const QuizCreation = (props: Props) => {
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
               <FormField
                 control={form.control}
-                name="username"
+                name="topic"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>Username</FormLabel>
+                    <FormLabel>Topic</FormLabel>
                     <FormControl>
-                      <Input placeholder="shadcn" {...field} />
+                      <Input placeholder="Enter a topic" {...field} />
                     </FormControl>
                     <FormDescription>
-                      This is your public display name.
+                      Please Provide a Topic
                     </FormDescription>
                     <FormMessage />
                   </FormItem>
                 )}
               />
+
+              <FormField
+                control={form.control}
+                name="amount"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Number of Questions</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="How many questions?"
+                        type="number"
+                        {...field}
+                        onChange={(e) => {
+                          form.setValue("amount", parseInt(e.target.value));
+                        }}
+                        min={1}
+                        max={30}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      You can choose how many questions you would like to be
+                      quizzed on here.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+
+              <div className="flex justify-between">
+                <Button
+                  variant={
+                    form.getValues("type") === "mcq" ? "default" : "secondary"
+                  }
+                  className="w-1/2 rounded-none rounded-l-lg"
+                  onClick={() => {
+                    form.setValue("type", "mcq");
+                  }}
+                  type="button"
+                >
+                  <CopyCheck className="w-4 h-4 mr-2" /> Multiple Choice
+                </Button>
+                <Separator  />
+                <Button
+                  variant={
+                    form.getValues("type") === "open_ended"
+                      ? "default"
+                      : "secondary"
+                  }
+                  className="w-1/2 rounded-none rounded-r-lg"
+                  onClick={() => form.setValue("type", "open_ended")}
+                  type="button"
+                >
+                  <BookOpen className="w-4 h-4 mr-2" /> Open Ended
+                </Button>
+              </div>
               <Button type="submit">Submit</Button>
             </form>
           </Form>
